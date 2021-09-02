d6192c6a48298679c721db3688eb878db32add89
Vcash <vadymkozak3@gmail.com>
2021-09-02 13:57:32 +0200

    functional, get links matched titles

--- match.py
+++ match.py
@@ -26,34 +26,44 @@ def make_match_data(item,attribute):
     for word in words:
         word = word.capitalize() 
         new_words.append(word)
-    item = ' '.join(new_words)
+    item_p = ' '.join(new_words)
 
-    if 'Iphone' in item:
-        item = item.replace('Iphone','iPhone')
+    if 'Iphone' or 'iphone' in item:
+        item_p = item.replace('Iphone','iPhone').replace('iphone','iPhone')
 
-    attribute = attribute.capitalize()
-    return item, attribute
+    attribute_p = attribute.capitalize()
+
+    print('maked data to match:',item_p,attribute_p)
+    return item_p, attribute_p
 
 
 def make_request(url):
     headers = {'user-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"}
     session = HTMLSession()
     r = session.get(url,headers=headers,allow_redirects=True)
+    print('request made')
     return r
 
 
-def get_matched_links(response):
+def get_matched_links(response,item_p,attribute_p,query):
     products_title = response.html.xpath('//div[@class="a-section a-spacing-none"]/div[@class="a-section a-spacing-none a-spacing-top-small"]/h2')
     #s = tag[0].text
     links = []
     for prod in products_title:
         #print(prod.text)
         if item_p and attribute_p in prod.text:
-            link = prod.absolute_links
-            #print({'query':query,'link':link})
-            #entry = {'query':query,'link':link,'prod_title':prod.text}
-            entry = (query,link,prod.text)
-            links.append(entry)
+            #if prod.text not in ['Carcasa', 'Funda', 'Protector', 'Soporte'] :
+            #if 'Carcasa' and 'Funda' and 'Protector' and 'Soporte' not in prod.text:
+            if 'Carcasa' not in prod.text and 'Funda' not in prod.text and 'Protector' not in prod.text and 'Soporte' not in prod.text:
+                link = prod.absolute_links
+                link = str(link)
+                link = link.replace('{','').replace('}','')
+                link = link.replace("'",'')
+                #print({'query':query,'link':link})
+                #entry = {'query':query,'link':link,'prod_title':prod.text}
+                entry = (query,prod.text,' ',link)
+                print(entry)
+                links.append(entry)
     
     return links
 
@@ -85,31 +95,44 @@ def write_excel(links):
     #     if row[0] != None:
     #         pass
 
+item_attribute_list = []
+def get_item_attribute():
+    wb = load_workbook(filename = 'phones_color_variations.xlsx')
+    ws = wb.active
+ 
+    for row in ws.iter_rows(values_only=True):
+        item = row[0]
+        attribute = row[1]
+        item_attribute_list.append({'item':item,'attribute':attribute})
 
-#take the item and its attribut from target_prods
-item = 'iPhone 12 pro' #Pro
-attribute = 'grafito'
+# item = 'iPhone 12 pro' #Pro
+# attribute = 'grafito'
+get_item_attribute()
+for element in item_attribute_list:
 
+    item = element.get('item')
+    attribute = element.get('attribute')
 
+    print(item,attribute)
 
-#process the item and the attribute to match Amazon's standart (Capitalization, iPhone,etc...)
-#This is used later to identify matches within the titles of the prods.
-# _p means processed: from 'iphone pro' to 'iPhone Pro'
-item_p,attribute_p = make_match_data(item,attribute)
+    #process the item and the attribute to match Amazon's standart (Capitalization, iPhone,etc...)
+    #This is used later to identify matches within the titles of the prods.
+    # _p means processed: from 'iphone pro' to 'iPhone Pro'
+    item_p,attribute_p = make_match_data(item,attribute)
 
-#with the above data make the url and the query (used later in the excel)
-url, query = make_query_url(item_p,attribute_p)
+    #with the above data make the url and the query (used later in the excel)
+    url, query = make_query_url(item_p,attribute_p)
 
-#make the request with the query
-response = make_request(url)
+    #make the request with the query
+    response = make_request(url)
 
-#extract the links of the products which titles matches the query
-#list of dicts with link , query, prod_title
-links = get_matched_links(response)
+    #extract the links of the products which titles matches the query
+    #list of dicts with link , query, prod_title
+    links = get_matched_links(response, item_p,attribute_p,query)
 
-# write excel with query , prod_title , selection, link
-#selection is if the human validate that url has the needed pictures
-write_excel(links)
+    # write excel with query , prod_title , selection, link
+    #selection is if the human validate that url has the needed pictures
+    write_excel(links)
 
 # r = make_request(url)
 # status = r.status_code
