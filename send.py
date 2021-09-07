9798e8e6b82feeb1955552b9e754d8f7cde02228
Vcash <vadymkozak3@gmail.com>
2021-09-07 18:37:07 +0200

    a

--- send.py
+++ send.py
@@ -2,5 +2,15 @@ from mega import Mega
 
 import login_file # file with MEGA user and password
 
-print(login_file.username)
+print(login_file.email)
 print(login_file.password)
+
+
+email = login_file.email
+password = login_file.password
+
+mega = Mega()
+
+m = mega.login(email, password)
+# login using a temporary anonymous account
+m = mega.login()
