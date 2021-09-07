554bc23009b18339de0d76dc92087dc126543b3d
my_name <my_email>
2021-09-07 18:32:32 +0200

    finished phones 2

--- missing_checker.py
+++ missing_checker.py
@@ -1,14 +1,31 @@
+from typing import Match
 from openpyxl.workbook.workbook import Workbook
 from openpyxl import load_workbook
 
+# list_a = ['iphone 12 pro max oro','iphone 12 pro max plata' ]
+# list_b = ['iphone 12 pro max oro' ]
+
+# for item in list_a:
+#     if item not in list_b:
+#         print(item)
+
+''' given 2 files on complete and other with missing items,
+ it creates a new file with the missing items
+ 
+ when invoek the functions below you can specify the column pf the data'''
+
+matches = 'phones_matches.xlsx'
+complete = 'phones_color_variations.xlsx'
+missing_items = 'phones_missing.xlsx'
+
 #given an excel file it returns a list of the values
-def read(filename):
+def read(filename, column=0):
     wb = load_workbook(filename = filename)
     ws = wb.active
 
     complete_list = []
     for row in ws.iter_rows(values_only=True):
-        item = row[0]
+        item = row[column]
         try:
             item = item.lower()
         except:
@@ -18,27 +35,22 @@ def read(filename):
     return complete_list
 
 #given 2 lists it writes the missing elements to a third file
-def write_missing(complete_list, incomplete_list, new_list_name):
+def write_missing(complete_list, incomplete_list):
     wb = Workbook()
     ws = wb.active
-
     n = 1
     for item in complete_list:
-        if item not in matches_list:
+        if item not in incomplete_list:
+            print('This item is missing in INcomplete_list:  ', item)
             ws.cell(row=n, column=1, value=item)
-        n += 1
+            n += 1
+    wb.save(missing_items)
     
-    wb.save(new_list_name)
-    
-#save file here
 
-matches = 'tablets_matches.xlsx'
-complete = 'tablets_color_variations.xlsx'
-
-complete_list = read(complete)
+complete_list = read(complete,column=2)
 matches_list = read(matches)
 
-write_missing(complete_list, matches_list, 'missing_tablets.xlsx')
+write_missing(complete_list, matches_list)
 
 
 
